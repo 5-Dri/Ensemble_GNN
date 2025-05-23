cf21e491b1d013f06d5862a61b5885fbe2c2e157
ri5_D <5d.enjoy.ri@gmail.com>
2025-05-24 00:25:26 +0900

    commit

--- prog/train_planetoid.py
+++ prog/train_planetoid.py
@@ -48,6 +48,74 @@ def test(data, model):
     return acc, alpha, whole_node_correct
 
 
+# def ensemble(cfg, data, device):
+
+#     model = load_net(cfg).to(device)
+#     optimizer = torch.optim.Adam(params       = model.parameters(), 
+#                                  lr           = cfg.learning_rate, 
+#                                  weight_decay = cfg.weight_decay)
+#     pickup_num = int(cfg.num_nodes * cfg.pickup_ratio)
+#     available_indices = torch.where(data.train_mask | data.val_mask | data.test_mask)[0]
+#     pred_sum = torch.zeros((cfg.num_nodes, cfg.n_class), device=device)
+    
+
+#     for _ in range(cfg.graphs_number):
+#         if cfg.strategy == "random":
+#             pickup_nodes = available_indices[torch.randperm(len(available_indices))[:pickup_num]]
+
+#         else:
+#             deg = degree(data.edge_index[0], num_nodes=cfg.num_nodes)
+#             deg = deg[available_indices]
+
+#             if cfg.strategy == "high_degree":
+#                 prob = deg + 1e-6
+#             elif cfg.strategy == "low_degree":
+#                 prob = 1.0 / (deg + 1e-6)
+#             else:
+#                 raise ValueError("Unknown strategy")
+            
+#             prob = prob / prob.sum()
+#             prob = prob.to(available_indices.device)
+#             pickup_nodes = available_indices[torch.multinomial(prob, pickup_num, replacement=False)].to(device)
+
+#         pickup_edge_index = build_pickup_edge_index(pickup_nodes, data, cfg)
+#         pickup_x = data.x[pickup_nodes]
+#         pickup_y = data.y[pickup_nodes]
+
+#         pickup_mask = index_to_mask(pickup_nodes, size=cfg.num_nodes)
+
+#         pickup_data = Data(
+#             x=pickup_x,
+#             y=pickup_y,
+#             edge_index=pickup_edge_index.to(device),
+#             train_mask=data.train_mask[pickup_mask],
+#             val_mask=data.val_mask[pickup_mask],
+#             test_mask=data.test_mask[pickup_mask]
+#         )
+#         early_stopping = EarlyStopping(patience=10)
+
+
+#         for epoch in range(1, cfg.epochs+1):
+#             loss_val, mini_acc_val = train(cfg, pickup_data, model, optimizer, device)
+#             early_stopping(loss_val, model)
+#             if early_stopping.early_stop:
+#                 break
+            
+#         model.eval()
+#         out, _ = model(pickup_data.x, pickup_data.edge_index)
+#         spread_pred = torch_spread(cfg, out, pickup_nodes, data, alpha=1.0, max_distance=3)
+#         log_pred = F.log_softmax(spread_pred, dim=1)
+#         pred_sum += log_pred
+
+#     final_log_pred = pred_sum / cfg.graphs_number
+#     pred_class = final_log_pred.argmax(dim=1)
+#     correct_val = (pred_class[data.val_mask] == data.y[data.val_mask]).sum().item()
+#     correct_test = (pred_class[data.test_mask] == data.y[data.test_mask]).sum().item()
+#     acc_val = correct_val / data.val_mask.sum().item()
+#     acc_test = correct_test / data.test_mask.sum().item()
+
+#     return acc_val, acc_test
+
 def ensemble(cfg, data, device):
 
     model = load_net(cfg).to(device)
@@ -55,28 +123,44 @@ def ensemble(cfg, data, device):
                                  lr           = cfg.learning_rate, 
                                  weight_decay = cfg.weight_decay)
     pickup_num = int(cfg.num_nodes * cfg.pickup_ratio)
-    available_indices = torch.where(data.train_mask | data.val_mask | data.test_mask)[0]
     pred_sum = torch.zeros((cfg.num_nodes, cfg.n_class), device=device)
-    
+
+    train_indices = torch.where(data.train_mask)[0].to(device)
+    other_indices = torch.where(data.val_mask | data.test_mask)[0].to(device)
+
+    train_pickup_num = int(pickup_num * len(train_indices) / cfg.num_nodes)
+    other_pickup_num = pickup_num - train_pickup_num
 
     for _ in range(cfg.graphs_number):
         if cfg.strategy == "random":
-            pickup_nodes = available_indices[torch.randperm(len(available_indices))[:pickup_num]]
+            train_pickup = train_indices[torch.randperm(len(train_indices), device=device)[:train_pickup_num]]
+            other_pickup = other_indices[torch.randperm(len(other_indices), device=device)[:other_pickup_num]]
+            pickup_nodes = torch.cat([train_pickup, other_pickup])
 
         else:
-            deg = degree(data.edge_index[0], num_nodes=cfg.num_nodes)
-            deg = deg[available_indices]
+            deg = degree(data.edge_index[0], num_nodes=cfg.num_nodes).to(device)
 
+            # trainノードから選ぶ
+            train_deg = deg[train_indices]
             if cfg.strategy == "high_degree":
-                prob = deg + 1e-6
+                train_prob = train_deg + 1e-6
             elif cfg.strategy == "low_degree":
-                prob = 1.0 / (deg + 1e-6)
+                train_prob = 1.0 / (train_deg + 1e-6)
             else:
                 raise ValueError("Unknown strategy")
-            
-            prob = prob / prob.sum()
-            prob = prob.to(available_indices.device)
-            pickup_nodes = available_indices[torch.multinomial(prob, pickup_num, replacement=False)].to(device)
+            train_prob = train_prob / train_prob.sum()
+            train_pickup = train_indices[torch.multinomial(train_prob, train_pickup_num, replacement=False)]
+
+            # その他ノードから選ぶ
+            other_deg = deg[other_indices]
+            if cfg.strategy == "high_degree":
+                other_prob = other_deg + 1e-6
+            elif cfg.strategy == "low_degree":
+                other_prob = 1.0 / (other_deg + 1e-6)
+            other_prob = other_prob / other_prob.sum()
+            other_pickup = other_indices[torch.multinomial(other_prob, other_pickup_num, replacement=False)]
+
+            pickup_nodes = torch.cat([train_pickup, other_pickup])
 
         pickup_edge_index = build_pickup_edge_index(pickup_nodes, data, cfg)
         pickup_x = data.x[pickup_nodes]
@@ -94,13 +178,12 @@ def ensemble(cfg, data, device):
         )
         early_stopping = EarlyStopping(patience=10)
 
-
         for epoch in range(1, cfg.epochs+1):
             loss_val, mini_acc_val = train(cfg, pickup_data, model, optimizer, device)
             early_stopping(loss_val, model)
             if early_stopping.early_stop:
                 break
-            
+
         model.eval()
         out, _ = model(pickup_data.x, pickup_data.edge_index)
         spread_pred = torch_spread(cfg, out, pickup_nodes, data, alpha=1.0, max_distance=3)
@@ -115,6 +198,8 @@ def ensemble(cfg, data, device):
     acc_test = correct_test / data.test_mask.sum().item()
 
     return acc_val, acc_test
+
+
 
 def train_and_test(cfg, data, device):
     model = load_net(cfg).to(device)
